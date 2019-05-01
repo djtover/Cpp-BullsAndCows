5c633d183e20cbff96f84ff7df1c952399a42372
David <djtover94@gmail.com>
2019-05-01 13:26:25 +0300

    Added bull counter to calculate

--- calculate.hpp
+++ calculate.hpp
@@ -3,9 +3,22 @@
 using namespace std;
 namespace bullpgia
 {
-   static string calculateBullAndPgia(string choice, string guess){
-        
-        return "4,0";
+static string calculateBullAndPgia(string choice, string guess)
+{
+
+    int bull = 0;
+    int pgia = 0;
+    int i;
+    for (i = 0; i < guess.length(); i++)
+    {
+        if (guess.at(i) == choice.at(i))
+        {
+            bull++;
+        }
     }
-    
+    cout << choice << "," << guess << "," << bull << "," << pgia << endl;
+    string ans = std::to_string(bull) + "," + std::to_string(pgia);
+    return ans;
+}
+
 }; // namespace bullpgia
